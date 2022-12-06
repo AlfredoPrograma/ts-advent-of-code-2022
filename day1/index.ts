c677b36a95190f35559ee41c68d247440006d3e0
AlfredoPrograma <alfredoprograma.dev@gmail.com>
2022-12-06 19:30:05 -0400

    AoC: first day challenge solution

---
+++ day1/index.ts
@@ -0,0 +1,39 @@
+const { readFileSync } = Deno
+
+// Setup for read input file
+const decoder = new TextDecoder('utf-8');
+const fileInput = readFileSync('input.txt');
+const inputString = decoder.decode(fileInput)
+
+// Divide input by groups
+const splittedData = inputString.split('\n')
+
+const maxCaloriesGroupsTotal: number[] = []
+let currentAcc = 0
+
+splittedData.forEach((val) => {
+  if (val === '') {
+    maxCaloriesGroupsTotal.push(currentAcc)
+    currentAcc = 0
+    return
+  }
+
+  const parsedVal = Number(val)
+  currentAcc += parsedVal
+})
+
+// Get top N numbers values recursively
+const getTopNumbers = (numbersArr: number[], topNumber: number, maximums: number[] = []): number[] => {
+  if (maximums.length === topNumber) return maximums
+
+  const removedCalculatedValues = numbersArr.filter(value => !maximums.includes(value))
+  const maxVal = Math.max(...removedCalculatedValues)
+
+  return getTopNumbers(numbersArr, topNumber, [...maximums, maxVal])
+}
+
+const topThreeCalories = getTopNumbers(maxCaloriesGroupsTotal, 3)
+const totalTopThreeCalories = topThreeCalories.reduce((acc, val) => acc + val, 0)
+
+// Result
+console.log(totalTopThreeCalories)
