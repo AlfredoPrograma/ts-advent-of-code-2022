5f754dee73fec4b3a12efda2ab65c4ec4451cf8a
AlfredoPrograma <alfredoprograma.dev@gmail.com>
2022-12-06 22:24:08 -0400

    AoC: second challenge solution code

---
+++ day2/index.ts
@@ -0,0 +1,55 @@
+const { readFileSync } = Deno;
+
+const decoder = new TextDecoder('utf-8');
+const fileInput = readFileSync('input.txt');
+const inputString = decoder.decode(fileInput)
+
+const splittedData = inputString.split('\n')
+
+type Shape = 'ROCK' | 'PAPER' | 'SCISSORS'
+interface ShapeRelation {
+  wins: Shape,
+  loses: Shape,
+  draw: Shape
+  value: number
+}
+
+const OPPONENT_SHAPES_MAP: Record<string, Shape> = {
+  'A': 'ROCK',
+  'B': 'PAPER',
+  'C': 'SCISSORS'
+}
+
+const OPPONENT_RESULT_MAP: Record<string, keyof ShapeRelation> = {
+  'X': 'wins',
+  'Y': 'draw',
+  'Z': 'loses'
+}
+
+const SHAPES_RELATION: Record<string, ShapeRelation> = {
+  'ROCK': { wins: 'SCISSORS',  loses: 'PAPER', draw: 'ROCK', value: 1 },
+  'PAPER': { wins: 'ROCK',  loses: 'SCISSORS', draw: 'PAPER', value: 2 },
+  'SCISSORS': { wins: 'PAPER',  loses: 'ROCK', draw: 'SCISSORS', value: 3 }
+}
+
+let totalPoints = 0
+
+for (const line of splittedData) {
+  const [o, u] = line.split(' ')
+  const [opponentShape, turnResult] = [OPPONENT_SHAPES_MAP[o], OPPONENT_RESULT_MAP[u]]
+
+  const userShape = SHAPES_RELATION[opponentShape][turnResult]
+
+  const { wins: winAgainst, loses: loseAgainst } = SHAPES_RELATION[userShape]
+
+  let roundPoints = SHAPES_RELATION[userShape].value
+
+  const roundValueMap = {
+    [winAgainst]: 6,
+    [userShape]: 3,
+    [loseAgainst]: 0
+  }
+
+  roundPoints += roundValueMap[opponentShape]
+  totalPoints += roundPoints 
+}
